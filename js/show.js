22eb942426c0032be0cccf1d0541b36af2ca9dc8
ibrahem hassan <hibrahem266@gmail.com>
2022-11-07 13:37:18 +0200

    alwatania

--- js/show.js
+++ js/show.js
@@ -1,6 +1,6 @@
 const card =document.getElementById("flexRadioDefault2");
 const info_card =document.getElementById("pay-info");
-
+const lang = document.getElementById("change-lang");
 window.onchange=()=>{
 
 if(card.checked){
@@ -10,3 +10,7 @@ else {
     info_card.style.display="none"
 }
 }
+
+
+
+
