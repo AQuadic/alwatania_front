88074e8f9c9459115a2b66cffd3c5abc2dff3ec0
Saifallak <Saifallak@yahoo.com>
2023-05-08 15:38:28 +0300

    update css.

--- js/card.js
+++ js/card.js
@@ -1,34 +1,14 @@
-const cash =document.getElementsByClassName("cash");
-const cardinfo =document.getElementsByClassName("pay-info1");
-
-
-
 
 window.onchange=function(){
-    if(cash[0].checked){
-        cardinfo[0].style.display="none";
-       
-       }
-       else{
-           cardinfo[0].style.display="block";
-       }
+       var x = document.getElementById("customRange1").value;
+
+document.getElementById("valuerang").innerHTML = x;
+
 };
 
 
 
 
 
-const addtv=document.getElementById("tv-add");
-const deltv=document.getElementById("tv-del");
-const numtv=document.getElementById("num-tv");
 
-addtv.onclick=function(){
-    numtv.innerHTML=+numtv.innerHTML+1
-}
-deltv.onclick=function(){
-    if(+numtv.innerHTML>0){
-        numtv.innerHTML=+numtv.innerHTML-1
-    }
-  
-   
-}
+
